970416bdafdc63f8f90db6d0a9c2d86a00345b0d
pychua <chua86614@gmail.com>
2024-11-12 14:11:35 +0800

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import {formatCurrency} from '../utils/money.js';
 // import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; // default export
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
 // hello();
 
@@ -114,6 +115,8 @@ export function renderOrderSummary() {
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
             updateCartQuantity();
+
+            renderPaymentSummary();
         });
     });
 
@@ -138,7 +141,8 @@ export function renderOrderSummary() {
             
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.classList.remove('is-editing-quantity');
-            
+
+            renderPaymentSummary();
         });
     });
 
@@ -152,6 +156,7 @@ export function renderOrderSummary() {
             const { productId, deliveryOptionId } = element.dataset
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
