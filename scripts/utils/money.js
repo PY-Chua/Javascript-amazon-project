fd9f858ab767774bc3a07cfcf607aee97bd9c680
pychua <chua86614@gmail.com>
2024-11-12 13:21:19 +0800

    Save data and generate HTML for delivery options

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,8 @@
 export function formatCurrency(priceCents) {
     return (priceCents / 100).toFixed(2)
 }
+
+// each file can only have 1 default export
+// default export use no curly bracket when importing 
+// e.g import formatCurrency from './utils/money.js';
+// export default formatCurrency;
