5fac6d205d2daf63e9468b926429f789b250c813
pychua <chua86614@gmail.com>
2024-11-14 11:03:53 +0800

    Learn error handling

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,21 +6,30 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
 
-async function loadPage() {    
-    await loadProductsFetch();
+async function loadPage() {
+    try {
+        // throw 'error1';
 
-    const value = await new Promise((resolve) => {
-        loadCart(() => {
-            resolve('value3');
+        await loadProductsFetch();
+
+        const value = await new Promise((resolve, reject) => {
+            // throw 'error2';
+            loadCart(() => {
+                // reject('error3');
+                resolve('value3');
+            });
         });
-    });
 
+    } catch (error) {
+        console.log('Unexpected error. Please try again later.');
+    }
+    
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
 
 }
-loadPage()
+loadPage();
 
 /*
 Promise.all([
